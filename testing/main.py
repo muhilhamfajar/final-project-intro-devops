df185efdfc868e0f3325cd60c8b733a509255363
Muhammad Ilham Fajar <fajarmuhammadilham@gmail.com>
2023-12-30 19:27:05 +0700

    Update main.py

--- testing/main.py
+++ testing/main.py
@@ -1,36 +1,25 @@
-import time
-import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-class TestHomePage:
-    @classmethod
-    def setup_class(cls):
-        # Initialize WebDriver with headless mode
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        cls.driver = webdriver.Chrome(options=chrome_options)
-        cls.driver.maximize_window()
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-    @classmethod
-    def teardown_class(cls):
-        # Close the browser after the test
-        cls.driver.quit()
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
 
-    def test_open_page(self):
-        # Open homepage
-        self.driver.get("http://frontend:8080/")
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-        # Wait for the results page to load
-        time.sleep(2)
-
-        # Get the title of the page
-        page_title = self.driver.title
-
-        expected_title = "Tweet App" 
-        assert expected_title in page_title, f"Expected title '{expected_title}' not found in actual title '{page_title}'"
-
-# Run the test if the script is executed directly
-if __name__ == "__main__":
-    pytest.main(["-v", "main.py"])
+driver.get('http://nytimes.com')
+print(driver.title)
