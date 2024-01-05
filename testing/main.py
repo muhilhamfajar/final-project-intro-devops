9275ace376938f77bad8fc26f7f8986746e4bd52
Muhammad Ilham Fajar <fajarmuhammadilham@gmail.com>
2024-01-05 19:41:47 +0700

    Update testing.yaml (#2)

--- testing/main.py
+++ testing/main.py
@@ -3,20 +3,34 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
 
 class TestHomePage:
     @classmethod
     def setup_class(cls):
-        # Initialize WebDriver with headless mode
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        cls.driver = webdriver.Chrome(options=chrome_options)
-        cls.driver.maximize_window()
+        # Start virtual display
+        cls.display = Display(visible=0, size=(800, 800))
+        cls.display.start()
+
+        # Install ChromeDriver
+        chromedriver_autoinstaller.install()
+
+        # Set up Chrome options
+        cls.chrome_options = webdriver.ChromeOptions()
+        cls.chrome_options.add_argument("--window-size=1200,1200")
+        cls.chrome_options.add_argument("--ignore-certificate-errors")
+        cls.chrome_options.add_argument("--headless")  # Add headless option
+
+        # Initialize WebDriver
+        cls.driver = webdriver.Chrome(options=cls.chrome_options)
 
     @classmethod
     def teardown_class(cls):
-        # Close the browser after the test
+        # Close the browser and stop virtual display
         cls.driver.quit()
+        cls.display.stop()
 
     def test_open_page(self):
         # Open homepage
@@ -29,6 +43,19 @@ class TestHomePage:
         page_title = self.driver.title
 
         expected_title = "Tweet App" 
+        assert expected_title in page_title, f"Expected title '{expected_title}' not found in actual title '{page_title}'"
+
+    def test_github_page(self):
+        # Open GitHub homepage
+        self.driver.get("http://github.com")
+
+        # Wait for the results page to load
+        time.sleep(2)
+
+        # Get the title of the page
+        page_title = self.driver.title
+
+        expected_title = "The world's leading software development platform" 
         assert expected_title in page_title, f"Expected title '{expected_title}' not found in actual title '{page_title}'"
 
 # Run the test if the script is executed directly
